2d4381064a0c401ac7ad634d36c3cd0fe4dd320e
Krishnendhu-Pu <krishnendhupathiyoor@gmail.com>
2025-11-06 17:33:24 +0000

    success push from intellij

--- src/main/java/com/example/HotelBookingSystem/dto/BookingRequest.java
+++ src/main/java/com/example/HotelBookingSystem/dto/BookingRequest.java
@@ -1,4 +1,15 @@
 package com.example.HotelBookingSystem.dto;
 
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class BookingRequest {
+
+    private String customerName;
+    private String roomType;
+    private LocalDateTime bookingDate;
+    private String status;
 }
--- src/main/java/com/example/HotelBookingSystem/dto/BookingResponse.java
+++ src/main/java/com/example/HotelBookingSystem/dto/BookingResponse.java
@@ -1,4 +1,21 @@
 package com.example.HotelBookingSystem.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
 public class BookingResponse {
+
+    private int id;
+    private String customerName;
+    private String roomType;
+    private LocalDateTime bookingDate;
+    private String status;
 }
