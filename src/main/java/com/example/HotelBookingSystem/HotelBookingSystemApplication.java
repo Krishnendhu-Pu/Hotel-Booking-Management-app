f7518a6688e6c56a2bdd62a99029d916eb81a45f
Krishnendhu-Pu <krishnendhupathiyoor@gmail.com>
2025-11-08 14:34:58 +0000

    backend code completed for booking

--- src/main/java/com/example/HotelBookingSystem/HotelBookingSystemApplication.java
+++ src/main/java/com/example/HotelBookingSystem/HotelBookingSystemApplication.java
@@ -3,7 +3,7 @@ package com.example.HotelBookingSystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.HotelBookingSystem")
 public class HotelBookingSystemApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/HotelBookingSystem/controller/BookingController.java
+++ src/main/java/com/example/HotelBookingSystem/controller/BookingController.java
@@ -1,16 +1,49 @@
 package com.example.HotelBookingSystem.controller;
 
 
+import com.example.HotelBookingSystem.dto.BookingRequest;
+import com.example.HotelBookingSystem.dto.BookingResponse;
 import com.example.HotelBookingSystem.services.BookingService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/app/bookings")
+@RequestMapping("/api/bookings")
 public class BookingController {
 
     private final BookingService bookingService;
+
+    @PostMapping
+    public ResponseEntity<BookingResponse> createBooking(@RequestBody BookingRequest request){
+        BookingResponse response = bookingService.createBooking(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BookingResponse>> getAllBookings(){
+        return ResponseEntity.ok(bookingService.getAllBookings());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookingResponse> getBookingById(@PathVariable int id){
+        return ResponseEntity.ok(bookingService.getBookingById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BookingResponse> updateBooking(@PathVariable int id, @RequestBody BookingRequest request){
+        return ResponseEntity.ok(bookingService.updateBooking(id,request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBooking(@PathVariable int id){
+       bookingService.deleteBooking(id);
+       return ResponseEntity.noContent().build();
+    }
 
 }
